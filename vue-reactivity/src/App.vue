bddab3ae142fc383a946216fb112482a8762809b
Malak37 <71739461+Malak37@users.noreply.github.com>
2021-10-21 12:29:19 -0400

    commit

--- vue-reactivity/src/App.vue
+++ vue-reactivity/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <Sandwich msg="Welcome to Your Vue.js App" />
+    <Sandwich msg="Make your Sandwich!" />
   </div>
 </template>
 
--- vue-reactivity/src/components/Sandwich.vue
+++ vue-reactivity/src/components/Sandwich.vue
@@ -1,13 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
+    <p>Choose your bread, protein, and veggies s well as any other toppings.</p>
     <h3>Installed CLI Plugins</h3>
     <ul>
       <li>
